d44058682615bcf18e1d26ce4f3727f394959bf0
daniel031 <danielmaldonadogutierrez031@gmail.com>
2026-09-18 09:42:55 -0400

    feat: módulo Accounts con CRUD REST y validación

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { AccountsModule } from './modules/accounts/accounts.module';
 import { JwtAuthGuard } from './common/jwt-auth.guard';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    AccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: JwtAuthGuard }],
 })
